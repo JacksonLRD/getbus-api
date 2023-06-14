ee9cd61b41315d3c2eeea088784c716e2c4d4f24
Jackson <jacksonlrdsantos@gmail.com>
2023-06-14 20:11:52 -0300

    feat: finaliza metodo para fazer roteamento

--- src/shared/router/index.js
+++ src/shared/router/index.js
@@ -1,7 +1,7 @@
 import http from "node:http";
 import { DEFAULT_HEADER } from "../utils/defaultHeader.js";
 
-export default function peri() {
+export default function customRouter() {
   const routes = {
     default: (request, response) => {
       response.writeHead(404, DEFAULT_HEADER);
@@ -10,60 +10,49 @@ export default function peri() {
     },
   };
 
+  function listen(port, cb) {
+    http.createServer(_handler).listen(port, cb());
+  }
+
+  function get(path, controller) {
+    const route = path.concat(":", "get");
+
+    routes[route] = controller;
+  }
+
+  function post(path, controller) {
+    const route = path.concat(":", "post");
+
+    routes[route] = controller;
+  }
+
+  function _handler(req, res) {
+    const { url, method } = req;
+    const pathName = url.split("?")[0];
+    const key = `${pathName}:${method.toLowerCase()}`;
+
+    const route = routes[key] || routes.default;
+
+    return Promise.resolve(route(req, res)).catch(_ErrorHandler(res));
+  }
+
+  function _ErrorHandler(response) {
+    return (error) => {
+      console.error("FATAL ERROR", error);
+      response.writeHead(500, DEFAULT_HEADER);
+      response.write(
+        JSON.stringify({
+          error: "Internal Server Error",
+        })
+      );
+
+      return response.end();
+    };
+  }
+
   return {
-    listen(port) {
-      http.createServer(this._handler).listen(port);
-    },
-
-    /**
-     *
-     * @param {string} path
-     * @param {*} controller
-     */
-    get(path, controller) {
-      console.log(path);
-      const pathN = path.concat(":", "get");
-
-      Object.defineProperty(routes, pathN, {
-        value: controller,
-        writable: false,
-      });
-    },
-
-    post(path, controller) {
-      const pathN = path.concat(":", "post");
-
-      Object.defineProperty(routes, pathN, {
-        value: controller,
-        writable: false,
-      });
-    },
-
-    _handler(req, res) {
-      const { url, method } = req;
-      const pathName = url.split("?")[0];
-      const key = `${pathName}:${method.toLowerCase()}`;
-
-      console.log("routes\n", routes);
-
-      const route = routes[key] || routes.default;
-      console.log(route);
-
-      return Promise.resolve(route(req, res)).catch(handleError(res));
-    },
-  };
-}
-
-function handleError(response) {
-  return (error) => {
-    console.error("FATAL ERROR", error.stack);
-    response.writeHead(500, DEFAULT_HEADER);
-    response.write(
-      JSON.stringify({
-        error: "Internal Server Error",
-      })
-    );
-
-    return response.end();
+    listen,
+    get,
+    post,
   };
 }
