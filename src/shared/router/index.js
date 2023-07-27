b69f7cf7d4275b2d354c05a314ac9c26c47f05d9
Jackson <jacksonlrdsantos@gmail.com>
2023-07-26 21:59:29 -0300

    refactor: altera _handler para lidar com rotas com params id

--- src/shared/router/index.js
+++ src/shared/router/index.js
@@ -3,20 +3,19 @@ import http from "node:http";
 import { DEFAULT_HEADER } from "../utils/defaultHeader.js";
 import debug from "../utils/debug.js";
 
-  const httpVerbs = ["GET", "POST", "PUT", "DELETE", "PATCH"];
-  const routes = {
-    'default': (request, response) => {
-      response.writeHead(404, DEFAULT_HEADER);
-      response.write("Not Found");
-      response.end();
-    },
-  };
+const httpVerbs = ["GET", "POST", "PUT", "DELETE", "PATCH"];
+const routes = {
+  default: (request, response) => {
+    response.writeHead(404, DEFAULT_HEADER);
+    response.write("Not Found");
+    response.end();
+  },
+};
 
 /**
  * Creates a customRouter application
  */
 export default function CustomRouter() {
-
   /**
    * Starts a server listening for connections
    * @param {number | undefined} port Port to run an application
@@ -29,14 +28,13 @@ export default function CustomRouter() {
   function _handler(req, res) {
     const { url, method, headers } = req;
 
-    const parsedURL = new URL(url, `http://${headers.host}`)
+    const { pathname } = new URL(url, `http://${headers.host}`);
 
-
-    const pathName = url.split("?")[0];
-    const key = `${pathName}:${method}`;
+    const pathSanitized = pathname.split("/", 2).join("/");
+    const key = `${pathSanitized}:${method}`;
 
     const route = routes[key] || routes.default;
-    debug().log(`->src/shared/router/index.js\n ->Route called:\n  ${key}`)
+    debug().log(`->src/shared/router/index.js\n ->Route called:\n  ${key}`);
 
     return Promise.resolve(route(req, res)).catch(_ErrorHandler(res));
   }
@@ -55,7 +53,7 @@ export default function CustomRouter() {
     };
   }
 
-  void function _addMethods() {
+  void (function _addMethods() {
     for (let method of httpVerbs) {
       CustomRouter.prototype[method] = function (path, handler) {
         const route = path.concat(":", method);
@@ -63,9 +61,10 @@ export default function CustomRouter() {
         routes[route] = handler;
       };
     }
-  }()
+  })();
 
-  const { GET, POST, PUT, PATCH, DELETE } = CustomRouter.prototype
+  const { GET, POST, PUT, PATCH, DELETE } = CustomRouter.prototype;
+
   return {
     get: GET,
     post: POST,
@@ -73,5 +72,5 @@ export default function CustomRouter() {
     patch: PATCH,
     delete: DELETE,
     listen,
-  }
+  };
 }
